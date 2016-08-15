1578fd97deb8e2a006123aa5cdf8d5e77ec7a9a9
Mozart <mozart@olbrychtpalmer.net>
2016-08-15 17:25:04 +1000

    Removed redundant bits
    Fixed some of my derp.

--- build_all_draft.sh
+++ build_all_draft.sh
@@ -3,14 +3,14 @@
 for filename in part/*.wik
   do
   	echo !!! processing $filename ...
-    pandoc $filename -f mediawiki -t latex --standalone -o ${filename%.*}.tex
+    pandoc $filename -f mediawiki -t latex -o ${filename%.*}.tex
     echo ... converted $filename to TeX.
   done
   
 for filename in section/*.wik
   do
   	echo !!! processing $filename ...
-    pandoc $filename -f mediawiki -t latex --standalone -o ${filename%.*}.tex
+    pandoc $filename -f mediawiki -t latex -o ${filename%.*}.tex
     echo ... converted $filename to TeX.
   done
   
@@ -18,8 +18,8 @@ if [ "$(ls part/*.tex)" ]; then
   for filename in part/*.tex
     do
       echo !!! processing $filename ...
-  	  title=$(sed -n '52p' $filename)
-      sed -i '' -e '1,53d' $filename
+  	  title=$(sed -n '1p' $filename)
+      sed -i '' -e '1,2d' $filename
       sed -i '' -e 's/\\end{document}//g;s/\\label.*}//g;s/\\\tightlist//g' $filename
       sed -i '' -e 's/\\section/\\part/g;s/\\subsection/\\section/g;s/\\subsubsection/\\subsection/g' $filename
 	  python3 convert_tex_to_html.py > ${filename%.*}.html -f $filename -P -T "$title" | sed 's/ /\\ /g'
@@ -31,8 +31,8 @@ if [ "$(ls section/*.tex)" ]; then
   for filename in section/*.tex
     do
       echo !!! processing $filename ...
-      title=$(sed -n '52p' $filename)
-      sed -i '' -e '1,53d' $filename
+  	  title=$(sed -n '1p' $filename)
+      sed -i '' -e '1,2d' $filename
       sed -i '' -e 's/\\end{document}//g;s/\\label.*}//g;s/\\\tightlist//g' $filename
       sed -i '' -e '1s/^/\\part{}\
 /' $filename
